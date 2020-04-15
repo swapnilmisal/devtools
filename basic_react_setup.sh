81f4967d417ad69360dc7cc682377fbdc5c3c103
Swapnil Misal <swapnil.misal@haptik.ai>
2020-04-16 02:01:23 +0530

    load nvm in the script using source

--- basic_react_setup.sh
+++ basic_react_setup.sh
@@ -4,6 +4,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
+source ~/.bashrc
 nvm install 10.16.3
 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -

980f86c1d3cca797adb1378bc0de9fc5c9295fa4
Swapnil Misal <swapnil.misal@gmail.com>
2020-04-15 11:39:35 +0530

    create basic_react_setup.sh
    Basic React setup for ubuntu

---
+++ basic_react_setup.sh
@@ -0,0 +1,12 @@
+# for ubuntu
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+nvm install 10.16.3
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt update && sudo apt install --no-install-recommends yarn

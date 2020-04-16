86a8a96f8aa94fe0586e7a18aa632f6050d123e1
Swapnil Misal <swapnil.misal@gmail.com>
2020-04-16 15:18:25 +0530

    use installed node

--- basic_react_setup.sh
+++ basic_react_setup.sh
@@ -11,3 +11,5 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 sudo apt update && sudo apt install --no-install-recommends yarn
+
+nvm use 10.16.3

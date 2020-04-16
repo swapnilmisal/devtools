9a664db876742a6c610a4b44a82a92d87e9e5743
Swapnil Misal <swapnil.misal@gmail.com>
2020-04-16 15:19:37 +0530

    reload bashrc

--- react_build_test.sh
+++ react_build_test.sh
@@ -1,4 +1,5 @@
 curl -o- https://raw.githubusercontent.com/swapnilmisal/devtools/980f86c1d3cca797adb1378bc0de9fc5c9295fa4/basic_react_setup.sh | bash
 git clone https://github.com/hellohaptik/relements.git
+soure ~/.bashrc
 ( cd relements && yarn )
 ( cd relements && yarn build )
